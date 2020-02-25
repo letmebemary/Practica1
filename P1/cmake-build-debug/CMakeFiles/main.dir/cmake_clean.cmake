adfe9761bf0b484e2f5022196dd9df065f758c7c
jeropardor <61469636+jeropardor@users.noreply.github.com>
2020-02-25 18:04:17 +0100

    Add files via upload

---
+++ P1/cmake-build-debug/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../bin/main.exe"
+  "../bin/main.pdb"
+  "CMakeFiles/main.dir/main.c.o"
+  "libmain.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ P1/cmake-build-debug/CMakeFiles/test_lists.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../bin/test_lists.exe"
+  "../bin/test_lists.pdb"
+  "CMakeFiles/test_lists.dir/test.c.o"
+  "libtest_lists.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/test_lists.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
